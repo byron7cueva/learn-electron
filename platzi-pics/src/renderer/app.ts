52e070bf1cd93d8df55a8454d0434ac28708f808
byron7cueva <byron7cueva@gmail.com>
2020-09-21 14:45:15 -0500

    Filter image

--- platzi-pics/src/renderer/app.ts
+++ platzi-pics/src/renderer/app.ts
@@ -1,12 +1,16 @@
 import 'photonkit/dist/css/photon.css';
 import '../assets/css/main.css';
 
+import url, {Url} from 'url';
+import path from 'path';
+
 type HTMLImageElementOrNull = HTMLImageElement | null;
+type HTMLElementOrNull = HTMLElement | null;
 
 /**
  * Add the events to image list
  */
-function addImageEvents() {
+function addImageEvents(): void {
   const thumbs: NodeListOf<HTMLElement> = document.querySelectorAll('li.list-group-item');
   thumbs.forEach(thumb => {
     thumb.addEventListener('click', function() {
@@ -20,7 +24,8 @@ function addImageEvents() {
  * 
  * @param {HTMLElement} node li selected from list images
  */
-function changeImage(node: HTMLElement) {
+function changeImage(node: HTMLElementOrNull): void {
+  if (node === null) return;
   const liSelected: HTMLElement | null = document.querySelector('li.selected');
   if (liSelected) {
     liSelected.classList.remove('selected');
@@ -33,6 +38,47 @@ function changeImage(node: HTMLElement) {
   }
 }
 
+/**
+ * Filter the images
+ */
+function searchImagesEvent() {
+  const searchBox: HTMLInputElement | null = document.querySelector('#search-box');
+
+  if (searchBox) {
+    searchBox.addEventListener('keyup', function() {
+      const regex = new RegExp(this.value.toLowerCase(),'gi');
+      const thumbs: NodeListOf<HTMLImageElement> = document.querySelectorAll('li.list-group-item img');
+      if (this.value.length > 0) {
+        thumbs.forEach(thumb => {
+          const fileUrl: Url = url.parse(thumb.src);
+          const fileName = path.basename(fileUrl.pathname);
+          const parentNode = (thumb.parentNode as HTMLElement);
+          if (regex.exec(fileName)) {
+            parentNode.classList.remove('hidden');
+          } else {
+            parentNode.classList.add('hidden');
+          }
+        });
+      } else {
+        thumbs.forEach(thumb => {
+          const parentNode = (thumb.parentNode as HTMLElement);
+          parentNode.classList.remove('hidden');
+        });
+      }
+      selectFirstImage();
+    })
+  }
+}
+
+/**
+ * Select first image don't hidden
+ */
+function selectFirstImage() {
+  const image: HTMLElement = document.querySelector('li.list-group-item:not(.hidden)');
+  changeImage(image);
+}
+
 window.addEventListener('load', () => {
   addImageEvents();
+  searchImagesEvent();
 });
